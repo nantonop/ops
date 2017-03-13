2a0d9bb663c6ddd3723bebb91cb8940bc638b83e
nantonop <nikos.antonop@gmail.com>
2017-03-13 01:02:03 +0000

    first
    first creation

---
+++ deleter.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from lxml import objectify, etree
+import xmltodict
+from xml.dom import minidom
+import time
+import os
+import xml.etree.ElementTree as ET
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+import time
+i = 0
+
+def selectiveDeleter(path):
+    i = 0
+    for root, dirs, files in os.walk(path, topdown=False):
+        # print 'Files: '
+        for name in files:
+            #print(os.path.join(root, name))
+            #print 'Name: ', name, 'Root: ', root
+            filename = os.path.join(root, name)
+            extension = os.path.splitext(filename)[1]
+            filename_without_extension = os.path.splitext(name)[0]
+            if extension == '.zip':
+                print 'Deleting: ', filename
+                os.unlink(filename)
+
+if __name__ == "__main__":
+    global path
+    path = os.getcwd()
+    selectiveDeleter(path)
